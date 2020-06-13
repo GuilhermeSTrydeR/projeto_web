32c12097d26f3aa30f5f8961d1d1b77c9f6cc95b
Guilherme Silva <31942158+GuilhermeSTrydeR@users.noreply.github.com>
2020-06-13 02:08:42 -0300

    diversos arquivos
    foram incluídos nesse commit a pagina sobre e a pagina contato, ambas completas, no entanto a pagina contato ainda não possui conexão com o banco de dados.

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+
+$pagina = (isset($_GET['pagina'])) ? $_GET['pagina'] : 'inicio';
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -7,17 +13,40 @@
     <title>cadastro de pessoas</title>
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="./_css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
 
     <!-- CSS do projeto -->
-    <link rel="stylesheet" href="./_css/css.css">
+    <link rel="stylesheet" href="./css/css.css">
 
 
 </head>
 <body>
     
-
-
+      <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav">
+            <li class="nav-item <?= ($pagina == 'inicio')?'active':'' ?>">
+              <a class="nav-link" href="?pagina=inicio">Início</a>
+            </li>
+            <li class="nav-item <?= ($pagina == 'sobre')?'active':'' ?>">
+              <a class="nav-link" href="?pagina=sobre">Sobre</a>
+            </li>
+            <li class="nav-item <?= ($pagina == 'listagem')?'active':'' ?>">
+              <a class="nav-link" href="?pagina=listagem">listagem</a>
+            </li>
+            <li class="nav-item <?= ($pagina == 'contato')?'active':'' ?>">
+              <a class="nav-link" href="?pagina=contato">Contato</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      
+      <?php
+        include("./paginas/$pagina.php");
+      ?>
 
 </body>
 </html>
---
+++ paginas/sobre.php
@@ -0,0 +1,22 @@
+<html>
+
+<div id="apresentacao_site">
+
+    esse site foi desenvolvido como parte das exigencias do curso <br>
+    de ciência da computação da disciplina de programação e design para web, <br> 
+    da universidade unincor - vale do rio verde, <br>
+    sem qualquer fim lucrativo <br>
+    <br>
+    Guilherme Pereira de Mello Silva, formando em Ciência da Computação
+
+</div>
+
+<div id="img_sobre">
+
+    <img src="./imagens/sobre.jpg">
+ 
+</div>
+
+
+
+</html>
